c98ba6f528af0833cdff4e59fccbdbd34d3e5dcf
YTube <adx.informer@gmail.com>
2017-09-07 08:32:45 +0000

    Add driver interface

---
+++ src/CompressorInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace ImageCompressor;
+
+use SplFileInfo;
+
+interface CompressorInterface
+{
+	public function setOriginalFile(SplFileInfo $originalFile);
+
+    public function setDestination($destination);
+
+    public function setQuality($quality = 90);
+
+    public function compress();
+}
